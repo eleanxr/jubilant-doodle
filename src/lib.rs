de6ecd585b8ea6ffa1df0e5d7904f360ee460e52
Will Dicharry <wdicharry@gmail.com>
2016-11-06 08:22:28 -0700

    This compiles.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,8 @@ use rustc_serialize::json::Object;
 
 enum Metric {
     FloatMetric(f64),
-    IntMetric(i64)
+    IntMetric(i64),
+    UIntMetric(u64)
 }
 
 enum Column {
@@ -16,35 +17,40 @@ enum Column {
     MetricColumn(Metric)
 }
 
-fn convert_item(name: &String, value: &Json) -> (String, Column) {
+fn convert_item(name: &String, value: &Json) -> Option<(String, Column)> {
+    let key = name.clone();
     match value {
-        &Json::I64(number) => (String::new(name),
+        &Json::I64(number) => Some((key,
                               Column::MetricColumn(
-                                  Metric::IntMetric(number))),
-        &Json::F64(number) => (String::new(name),
+                                  Metric::IntMetric(number)))),
+        &Json::F64(number) => Some((key,
                               Column::MetricColumn(
-                                  Metric::FloatMetric(number)))
+                                  Metric::FloatMetric(number)))),
+        &Json::U64(number) => Some((key,
+                               Column::MetricColumn(
+                                   Metric::UIntMetric(number)))),
+        &Json::String(ref s) => Some((key,
+                             Column::DimensionColumn(s.clone()))),
+        _ => None
     }
 }
 
 fn map_object(object: &Object) -> HashMap<String, Column> {
     object.iter()
         .map(|(name, value)| convert_item(name, value))
+        .filter_map(|x| x)
         .collect()
 }
 
 fn map_record(record: &Json) -> Option<HashMap<String, Column>> {
-    if record.is_object() {
-        Some(map_object(record.as_object))
-    } else {
-        None
-    }
+    record.as_object().map(|o| map_object(o))
 }
 
 fn map_records(records: &Vec<Json>) -> HashMap<String, Vec<Column>> {
     let mut columns = HashMap::new();
 
     for record in records.iter() {
+        columns.
     }
 
     columns
